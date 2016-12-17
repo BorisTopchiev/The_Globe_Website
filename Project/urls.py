c07c118e39d8bb9136eba5261cd7e14ce89ba713
BorisTopchiev <topcievboris97@gmail.com>
2016-12-17 13:54:31 +0200

    Simple login form added.

--- Project/urls.py
+++ Project/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^$', views.main_page, name='main_page'),
     url(r'^materials/$', views.materials_page, name='materials_page'),
     url(r'^registration/$', views.register_page, name='register_page'),
+    url(r'^login/$', views.login_page, name='login_page'),
     url(r'^admin/', admin.site.urls),
 ]
--- Project/views.py
+++ Project/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate, login, logout
 from sql_lib import SQLBase
 
 sqlDB = SQLBase()
@@ -20,6 +21,12 @@ def register_page(request):
 
     return render(request,'register_page.html')
 
+def login_page(request):
+    print "logining"
+    if request.method== 'POST':
+        return redirect('/')
+
+    return render(request, 'login_page.html')
 
 def materials_page(request):
     return render(request, 'materials_page.html')
